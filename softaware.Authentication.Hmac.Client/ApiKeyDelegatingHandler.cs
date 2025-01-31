120a4fd537dd99e20035db4f6d30902478567e1d
Behnam Mirzabeygi <behnammby@gmail.com>
2025-01-31 09:10:23 +0330

    Authentication lib added

---
+++ softaware.Authentication.Hmac.Client/ApiKeyDelegatingHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace softaware.Authentication.Hmac.Client
+{
+    public class ApiKeyDelegatingHandler : DelegatingHandler
+    {
+        private readonly string appId;
+        private readonly string apiKey;
+
+        public ApiKeyDelegatingHandler(string appId, string apiKey)
+        {
+            this.appId = !string.IsNullOrWhiteSpace(appId) ? appId : throw new ArgumentNullException(nameof(appId));
+            this.apiKey = !string.IsNullOrWhiteSpace(apiKey) ? apiKey : throw new ArgumentNullException(nameof(apiKey));
+
+            try
+            {
+                Convert.FromBase64String(this.apiKey);
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"{nameof(apiKey)} must be a valid base64 string.");
+            }
+        }
+
+        public ApiKeyDelegatingHandler(string appId, string apiKey, HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            this.appId = appId ?? throw new ArgumentNullException(nameof(appId));
+            this.apiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var requestContentBase64String = string.Empty;
+
+            var requestUri = WebUtility.UrlEncode(request.RequestUri.AbsoluteUri.ToLower());
+
+            var requestHttpMethod = request.Method.Method;
+
+            // Calculate UNIX time
+            var epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
+            var timeSpan = DateTime.UtcNow - epochStart;
+            var requestTimeStamp = Convert.ToUInt64(timeSpan.TotalSeconds).ToString();
+
+            // create random nonce for each request
+            var nonce = Guid.NewGuid().ToString("N");
+
+            // Checking if the request contains body, usually will be null wiht HTTP GET and DELETE
+            if (request.Content != null)
+            {
+                var content = await request.Content.ReadAsByteArrayAsync();
+                var md5 = MD5.Create();
+
+                // Hashing the request body, any change in request body will result in different hash, we'll incure message integrity
+                var requestContentHash = md5.ComputeHash(content);
+                requestContentBase64String = Convert.ToBase64String(requestContentHash);
+
+                // System.Console.WriteLine("Request content base 64 ======> {0}", requestContentBase64String);
+            }
+
+            // Creating the raw signature string
+            var signatureRawData = $"{this.appId}{requestHttpMethod}{requestUri}{requestTimeStamp}{nonce}{requestContentBase64String}";
+            // System.Console.WriteLine("Signature raw data =============> {0}", signatureRawData);
+
+            var apiKeyBytes = Convert.FromBase64String(this.apiKey);
+            var signature = Encoding.UTF8.GetBytes(signatureRawData);
+
+            using (var hmac = new HMACSHA256(apiKeyBytes))
+            {
+                var signatureBytes = hmac.ComputeHash(signature);
+                var requestSignatureBase64String = Convert.ToBase64String(signatureBytes);
+
+                // System.Console.WriteLine("Reuqest signature base 64 ========> {0}", requestSignatureBase64String);
+
+                // Setting the values in the Authorization header using custom scheme (Hmac)
+                request.Headers.Authorization = new AuthenticationHeaderValue(
+                    "Hmac",
+                    $"{this.appId}:{requestSignatureBase64String}:{nonce}:{requestTimeStamp}");
+            }
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            return response;
+        }
+    }
+}
